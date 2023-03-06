fd80d7346acb664fcc20033555a93d855e584d50
alex19k77 <122217034+alex19k77@users.noreply.github.com>
2023-03-06 13:31:19 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+// ДОМАШКА
+
+let a = 1;
+let b = "1";
+console.log(a == b);
+console.log(a === b);
+
+let a1 = 1;
+let b1 = true;
+
+console.log(a1 == b1);
+console.log(a1 === b1);
+
+let a2 = 1;
+let b2 = undefined;
+
+console.log(a2 == b2);
+console.log(a2 === b2);
+
+let a3 = 1;
+let b3 = null;
+
+console.log(a3 == b3);
+console.log(a3 === b3);
+
+let a4 = "1";
+let b4 = false;
+
+console.log(a4 == b4);
+console.log(a4 === b4);
+
+let a5 = "1";
+let b5 = undefined;
+
+console.log(a5 == b5);
+console.log(a5 === b5);
+
+let a6 = "1";
+let b6 = null;
+
+console.log(a6 == b6);
+console.log(a6 === b6);
+
+let a7 = true;
+let b7 = undefined;
+
+console.log(a7 == b7);
+console.log(a7 === b7);
+
+let a8 = true;
+let b8 = null;
+
+console.log(a8 == b8);
+console.log(a8 === b8);
+
+let a9 = undefined;
+let b9 = null;
+
+console.log(a9 == b9);
+console.log(a9 === b9);
